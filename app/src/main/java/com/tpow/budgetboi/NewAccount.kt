3addd9d1735e315db31a488b60e7ea270297cf2a
Tom Powers <17350105+tpow4@users.noreply.github.com>
2021-02-03 23:50:00 -0600

    Fragment base

--- app/src/main/java/com/tpow/budgetboi/NewAccount.kt
+++ app/src/main/java/com/tpow/budgetboi/NewAccount.kt
@@ -1,13 +1,12 @@
 package com.tpow.budgetboi
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
+import android.widget.EditText
+import com.google.android.material.button.MaterialButton
 
 class NewAccount : Fragment() {
 
@@ -16,17 +15,40 @@ class NewAccount : Fragment() {
     }
 
     private lateinit var viewModel: NewAccountViewModel
+    private lateinit var editInstitutionView: EditText
+    private lateinit var editAccountView: EditText
+    private val STARTING_BALANCE = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.new_account_fragment, container, false)
+        val fragmentView = inflater.inflate(R.layout.new_account_fragment, container, false)
+        editInstitutionView = fragmentView.findViewById(R.id.institutionEditText)
+        editAccountView = fragmentView.findViewById(R.id.accountEditText)
+
+        val button = fragmentView.findViewById<MaterialButton>(R.id.new_account_submit)
+        button.setOnClickListener {
+            //Todo: add fragment communication
+//            if (TextUtils.isEmpty(editInstitutionView.text) || TextUtils.isEmpty(editAccountView.text)) {
+//                setResult(Activity.RESULT_CANCELED, replyIntent)
+//            } else {
+//                val institution = editInstitutionView.text.toString()
+//                val account = editAccountView.text.toString()
+//                val accountList = arrayListOf<CharSequence>()
+//                accountList.add(institution)
+//                accountList.add(account)
+//                accountList.add(STARTING_BALANCE.toString())
+//                replyIntent.putCharSequenceArrayListExtra(EXTRA_NEW_ACCOUNT, accountList)
+//                setResult(Activity.RESULT_OK, replyIntent)
+//            }
+        }
+        return fragmentView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(NewAccountViewModel::class.java)
+        //viewModel = ViewModelProvider(this).get(NewAccountViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
