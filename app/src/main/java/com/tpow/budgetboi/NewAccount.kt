0d72fed16e8f753461080d8f6a0e8ae4d56daf76
Tom Powers <17350105+tpow4@users.noreply.github.com>
2021-02-04 00:56:09 -0600

    Implemented shareable viewmodel

--- app/src/main/java/com/tpow/budgetboi/NewAccount.kt
+++ app/src/main/java/com/tpow/budgetboi/NewAccount.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.google.android.material.button.MaterialButton
 
 class NewAccount : Fragment() {
@@ -14,42 +16,43 @@ class NewAccount : Fragment() {
         fun newInstance() = NewAccount()
     }
 
-    private lateinit var viewModel: NewAccountViewModel
+    private lateinit var viewModel: AccountViewModel
     private lateinit var editInstitutionView: EditText
     private lateinit var editAccountView: EditText
-    private val STARTING_BALANCE = 0
+    private lateinit var submissionButton : MaterialButton
+    private val STARTING_BALANCE = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val fragmentView = inflater.inflate(R.layout.new_account_fragment, container, false)
+
         editInstitutionView = fragmentView.findViewById(R.id.institutionEditText)
         editAccountView = fragmentView.findViewById(R.id.accountEditText)
+        submissionButton = fragmentView.findViewById<MaterialButton>(R.id.new_account_submit)
 
-        val button = fragmentView.findViewById<MaterialButton>(R.id.new_account_submit)
-        button.setOnClickListener {
-            //Todo: add fragment communication
-//            if (TextUtils.isEmpty(editInstitutionView.text) || TextUtils.isEmpty(editAccountView.text)) {
-//                setResult(Activity.RESULT_CANCELED, replyIntent)
-//            } else {
-//                val institution = editInstitutionView.text.toString()
-//                val account = editAccountView.text.toString()
-//                val accountList = arrayListOf<CharSequence>()
-//                accountList.add(institution)
-//                accountList.add(account)
-//                accountList.add(STARTING_BALANCE.toString())
-//                replyIntent.putCharSequenceArrayListExtra(EXTRA_NEW_ACCOUNT, accountList)
-//                setResult(Activity.RESULT_OK, replyIntent)
-//            }
-        }
         return fragmentView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //viewModel = ViewModelProvider(this).get(NewAccountViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(requireActivity(), AccountViewModelFactory((activity?.application as BudgetBoiApplication).repository)).get(AccountViewModel::class.java)
+        submissionButton.setOnClickListener { view ->
+            var isError = false
+            if (editInstitutionView.text.isBlank() || editAccountView.text.isBlank()) {
+                isError = true
+            }
+            else
+            {
+                val institution = editInstitutionView.text.toString()
+                val account = editAccountView.text.toString()
+                viewModel.insert(Account(0, account, institution, STARTING_BALANCE))
+            }
+
+            val action = NewAccountDirections.actionNewAccountToOverview(isError)
+            view.findNavController().navigate(action)
+        }
     }
 
 }
--- app/src/main/java/com/tpow/budgetboi/Overview.kt
+++ app/src/main/java/com/tpow/budgetboi/Overview.kt
@@ -6,7 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.compose.ui.viewinterop.viewModel
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavArgs
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Overview : Fragment() {
@@ -15,24 +23,49 @@ class Overview : Fragment() {
         fun newInstance() = Overview()
     }
 
+    val args: OverviewArgs by navArgs()
+
     private lateinit var viewModel: AccountViewModel
+    private lateinit var recyclerView : RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val fragmentView = inflater.inflate(R.layout.overview_fragment, container, false)
-        val button = fragmentView.findViewById<FloatingActionButton>(R.id.floatingActionButton)
-        button.setOnClickListener { view ->
+         recyclerView = fragmentView.findViewById(R.id.recyclerView)
+
+
+//        accountViewModel.allAccounts.observe(this, Observer { accounts ->
+//            accounts?.let { adapter.submitList(it) }
+//        })
+
+        val addItemButton = fragmentView.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        addItemButton.setOnClickListener { view ->
             view.findNavController().navigate(R.id.newAccount)
         }
+
         return fragmentView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //viewModel = ViewModelProvider(this, AccountViewModelFactory((activity?.application as BudgetBoiApplication).repository)).get(AccountViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(requireActivity(), AccountViewModelFactory((activity?.application as BudgetBoiApplication).repository)).get(AccountViewModel::class.java)
+        val adapter = AccountListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.addItemDecoration(AccountItemDecoration(resources.getDimensionPixelSize(R.dimen.account_vertical_margin)))
+        viewModel.allAccounts.observe(viewLifecycleOwner, { accounts ->
+            accounts?.let { adapter.submitList(it) }
+        })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(args.EntryError)
+        {
+            Toast.makeText(context, "Bruh, no text was entered", Toast.LENGTH_LONG). show()
+        }
     }
 
 }
