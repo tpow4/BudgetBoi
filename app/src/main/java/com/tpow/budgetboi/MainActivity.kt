06a779450dc610aa321c43520b760942a25f0f2e
Tom Powers <17350105+tpow4@users.noreply.github.com>
2021-02-03 22:34:17 -0600

    One activity scaffold

--- app/src/main/java/com/tpow/budgetboi/MainActivity.kt
+++ app/src/main/java/com/tpow/budgetboi/MainActivity.kt
@@ -2,10 +2,14 @@ package com.tpow.budgetboi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val navHostFragment = supportFragmentManager
+//            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController = navHostFragment.navController
     }
 }
---
+++ app/src/main/java/com/tpow/budgetboi/NewAccount.kt
@@ -0,0 +1,35 @@
+package com.tpow.budgetboi
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+class NewAccount : Fragment() {
+
+    companion object {
+        fun newInstance() = NewAccount()
+    }
+
+    private lateinit var viewModel: NewAccountViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.new_account_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NewAccountViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+
+
+}
